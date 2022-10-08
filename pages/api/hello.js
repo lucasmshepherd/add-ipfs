c97c2a69a9d3c8f7f89bb3e1ea283234d1fc1b81
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-10-08 10:57:40 -0700

    Added cert to project for HTTPS(SSL)

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    // Process a POST request
-  } else {
-    // Handle any other HTTP method
-  }
-}
