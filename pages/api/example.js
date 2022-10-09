f3e1a3ad7cdd02fa4fc11f2d1398d3fe0c143e13
Jared Shepherd <jared@webops.group>
2022-10-08 19:32:45 -0700

    adds async/await

--- pages/api/example.js
+++ pages/api/example.js
@@ -1,3 +1,12 @@
-export default function handler(req, res) {
-  res.status(200).json({ example: 'Test Resonser' })
+export default async function handler(req, res) {
+  const result = await resolveAfter2Seconds();
+  res.status(200).json({  result })
+}
+
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve('test async');
+    }, 2000);
+  });
 }
