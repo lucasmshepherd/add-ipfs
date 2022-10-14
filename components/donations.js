051df9a98de44d27075df4cce37b95a4901c36a3
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-10-13 18:01:38 -0700

    Donations page

--- components/donations.js
+++ components/donations.js
@@ -9,19 +9,19 @@ export default function Donations(props) {
     <>
       <div className={styles.donatebg}><div className={styles.donatebgglitch}>&nbsp;</div></div>
       <div className={styles.donations}>
-        <h2 className={styles.title}>DONATIONS</h2>
+        <h2 className={styles.heading}>DONATIONS</h2>
       </div>
       {/*<Image src={Banner} layout="responsive" priority="true" alt="Anarchist Computer Center" />*/}
       <div className={styles.content}>
-        <h3>Some title goes here</h3>
+        <h3 className={styles.title}>Some title goes here</h3>
 
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
         <hr className={styles.divider} />
-        <h3>Some title goes here</h3>
+        <h3 className={styles.title}>Some title goes here</h3>
 
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
         <hr className={styles.divider} />
-        <h3>Some title goes here</h3>
+        <h3 className={styles.title}>Some title goes here</h3>
 
         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.</p>
 
