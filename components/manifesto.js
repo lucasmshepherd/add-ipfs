6333a0b23e2ddfe7db5a162aad6f69a5b285fbda
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-10-24 02:59:16 -0700

    Manifesto content updated

--- components/manifesto.js
+++ components/manifesto.js
@@ -43,7 +43,46 @@ export default function Manifesto(props) {
         
         <blockquote>We believe that there is a path forward. It requires us ALL to cause a little bit of anarchy. We must be vigilant in our mission; we must trust the process.<br/><br/>Join our DAO. We would love to hear from you.</blockquote>
         
-        <a className="glitchme" data-text="// learn more \\" href="https://anarchistdevelopmentdao.gitbook.io/anarchist-development-dao/" rel="noreferrer" target="_blank">/ learn more \</a>
+        <h3 className="glitchme" data-text="Anarchist Development DAO">Anarchist Development DAO</h3>
+        
+        <p><i>The Anarchist Development DAO is here to accelerate the creation of a more decentralized society through direct fundraising and activism.</i></p>
+
+        <p>This is a new age. The State has failed. Corruption, violence, and oppression run rampant across the globe, from fascist dictatorships to so-called democracies. It is time to build something new…and now we finally have the right tools.</p>
+
+        <p>Where has centralization gotten us? To the edge of planetary oblivion. Anarchy speaks to the piece of us that wants to stand up against stagnant and tyrannical governments—and stand apart from the mainstream narratives of society.</p>
+
+        <p>The philosophy of anarchy has nested in the shadows for generations, but both proponents and potential anarchists alike lacked a true organizing and actionable process on a global scale—until now.</p>
+
+        <p>Decentralized Autonomous Organizations, DAOs, are already emulating a model of the anarchist philosophy. The Anarchist Development DAO brings together like-minded individuals who are using the technology of DeFi and crypto to escape institutional capture and implement real-world change.</p>
+
+        <h3 className="glitchme" data-text="The goals are radically simple:">The goals are radically simple:</h3>
+
+        <p>Generate and direct funding for oppressed, marginalized, or otherwise important movements or causes without the censorship of any central authority.</p>
+
+        <p>Educate and unite sovereign individuals in solidarity with causes around the world.</p>
+
+        <p>Build out the toolset of Web3, from cryptocurrencies & blockchain to smart contracts and communal governance, and provide an alternative to institutional forces.</p>
+
+        <h3 className="glitchme" data-text="Forget Occupy Wall Street. We are here to Occupy the Blockchain.">Forget Occupy Wall Street. We are here to Occupy the Blockchain.</h3>
+
+        <p>Already traders within the DeFi space already practice an unrealized form of Anarchism by defying the supremacy of archaic fiat currencies. The Anarchist Development DAO is ready to harness and hone this instinct and to fulfill the promise of ultimate freedom that Bitcoin and crypto first unleashed upon the world.</p>
+
+        <p>Enter the Anarchist Development Protocol protocol: the backbone of our cause. DAO members can submit proposals to fund new technology, investigations, protest movements, or other worthy causes in the pursuit of a more decentralized and healthy society.</p>
+
+        <p>While the DAO will eventually become <i>completely autonomous</i> in all aspects of governance and decision-making, the core ADD development team believes that real change starts with individuals taking action. This team will establish momentum, impact, and initial revenue streams to fund the DAO treasury in conjunction with community proposals.</p>
+
+        <h3 className="glitchme" data-text="Time to Begin">Time to Begin</h3>
+
+        <p>The DAO encourages all to do their own research. Join us if you are genuinely down with the mission of breaking free from centralized entities such as banks, state-sponsored violence, and monopolistic corporations.</p>
+ 
+        <p>From those who have been waiting their entire lives, to the skeptics who doubt it would ever be possible…</p>
+        
+        <p>Important ideas persist across time and anarchy’s time has finally come.</p>
+        
+        <p>While it may sometimes seem as if our world is ending, through collective effort we can redefine this moment as a new beginning.</p>
+        <br/>
+        <p><i>Let Anarchy Reign!</i></p>
+
       </div>
     </>
   )
