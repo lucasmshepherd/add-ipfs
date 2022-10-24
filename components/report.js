4594fd6a223ff75d9b817612cabfa515cb48da92
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-10-23 20:37:42 -0700

    Minor styling edits

--- components/report.js
+++ components/report.js
@@ -5,7 +5,17 @@ import parse from 'html-react-parser'
 import styles from '../styles/components/report.module.sass'
 
 export default function Report(props) {
-  let addPrice, apRate, totalStaked = 0
+  let addPrice = "0.00", 
+      apRate = "0%", 
+      totalStaked = "0.00", 
+      daySinceLaunch = 0, 
+      projectsFunded = 0, 
+      walletCount = 0, 
+      marketCap = 0.00, 
+      valueLocked = 0.00, 
+      ethSent = 0.00, 
+      treasuryBalance = 0.00, 
+      addBurned = 0.00
   let lock = (<svg className="icon-lock" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 135 135"><rect x="15" y="60" width="15" height="75"/><rect x="15" y="120" width="105" height="15"/><rect x="105" y="60" width="15" height="75"/><rect x="15" y="45.1" width="105" height="14.9"/><rect x="30" y="15" width="15" height="30"/><rect x="90" y="15" width="15" height="30"/><rect x="44.82" width="45.18" height="15"/><rect x="60" y="75" width="15" height="30"/></svg>)
   let page = props.page
   const connected = useSelector((state) => state.users.connected)
@@ -34,39 +44,31 @@ export default function Report(props) {
                 )
               }
             })()}
-        { !page == "stake" &&
+        { page == "dapp" &&
           <>
             <div className={styles.row}>
-              <div className={styles.cell}>days since project launch</div>
-              <div className={styles.cell}>0</div>
+              <div className={styles.cell}><h4>days_since_launch</h4><span>{connected == "true" ? daySinceLaunch : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>projects funded in ETH</div>
-              <div className={styles.cell}>0</div>
+              <div className={styles.cell}><h4>projects_funded_in_ETH</h4><span>{connected == "true" ? projectsFunded : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>citizen holder wallet count</div>
-              <div className={styles.cell}>0</div>
+              <div className={styles.cell}><h4>anarchist_wallet_count</h4><span>{connected == "true" ? walletCount : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>marketcap</div>
-              <div className={styles.cell}>{lock}</div>
+              <div className={styles.cell}><h4>marketcap</h4><span>{connected == "true" ? marketCap : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>total value locked</div>
-              <div className={styles.cell}>{lock}</div>
+              <div className={styles.cell}><h4>total_value_locked</h4><span>{connected == "true" ? valueLocked : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>total ETH sent</div>
-              <div className={styles.cell}>{lock}</div>
+              <div className={styles.cell}><h4>total_eth_sent</h4><span>{connected == "true" ? ethSent : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>treasury balance</div>
-              <div className={styles.cell}>{lock}</div>
+              <div className={styles.cell}><h4>treasury_balance</h4><span>{connected == "true" ? treasuryBalance : lock}</span></div>
             </div>
             <div className={styles.row}>
-              <div className={styles.cell}>ADD burned</div>
-              <div className={styles.cell}>{lock}</div>
+              <div className={styles.cell}><h4>ADD_burned</h4><span>{connected == "true" ? addBurned : lock}</span></div>
             </div>
           </>
         }
