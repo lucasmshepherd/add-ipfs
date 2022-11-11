b9c4fd885ae4be6ee7557dcdca0d331bce59b873
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-11-11 10:13:06 -0800

    Telegram update

--- components/social.js
+++ components/social.js
@@ -39,7 +39,7 @@ export default function Social(props){
             <Image layout="fixed" src={Icon2} priority="true" alt="Reddit" width="100" height="100" />
             <Image layout="fixed" src={IconBG} priority="true" alt="BG" width="100" height="100" />
           </a>
-          <a href="https://t.me/anarchistdevelopmentdao" target="_blank" rel="noreferrer" className={styles.link} onClick={soundEffect}>
+          <a href="https://t.me/fundanarchyportal" target="_blank" rel="noreferrer" className={styles.link} onClick={soundEffect}>
             <Image layout="fixed" src={Icon1} priority="true" alt="Telegram" width="100" height="100" />
             <Image layout="fixed" src={IconBG} priority="true" alt="BG" width="100" height="100" />
           </a>
