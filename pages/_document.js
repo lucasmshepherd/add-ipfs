928c2b08cee6c619cb622619ae6e0b8c7c559ca1
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-11-18 06:06:05 -0800

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -7,9 +7,9 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,700;1,400&display=swap" rel="stylesheet" />
-        <link rel="shortcut icon" href="/images/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon.png"/>
+        <link rel="shortcut icon" href="/assets/images/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon.png"/>
       </Head>
       <body>
         <div className="monitor-effect-overlay"></div>
