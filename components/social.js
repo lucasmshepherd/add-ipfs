8dd831c0092fc52eaf0aa4c6f11e746fe77609e4
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-11-19 20:56:33 -0800

    Added dextools to social links

--- components/social.js
+++ components/social.js
@@ -35,7 +35,7 @@ export default function Social(props){
     <> 
       <div className={styles.wrapper}>
         <div className={styles.social}>
-          <a href="mailto:info@fundanarchy.io" rel="noreferrer" className={styles.link} onClick={soundEffect}>
+          <a href="https://www.dextools.io/app/en/ether/pair-explorer/0x8fead6e1be5af7b5beda08688018d55079e6de35" rel="noreferrer" className={styles.link} onClick={soundEffect}>
             <Image layout="fixed" src={Icon2} priority="true" alt="Reddit" width="100" height="100" />
             <Image layout="fixed" src={IconBG} priority="true" alt="BG" width="100" height="100" />
           </a>
