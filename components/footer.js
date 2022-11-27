6ceb69b531eab2ee32d4f95d22df4efb1f4ec48e
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-11-27 13:39:11 -0800

    Add token contract

--- components/footer.js
+++ components/footer.js
@@ -8,6 +8,7 @@ export default function Footer() {
     <>
       <footer className={styles.footer}>
         <Container>
+          <a href="https://etherscan.io/token/0x6542c8f90915a57314f4167b81851a3cd731c6e2" target="_blank" rel="noreferrer"><b>Token Contract</b>0x6542c8f90915a57314f4167b81851a3cd731c6e2</a>
         </Container>
       </footer>
     </>
