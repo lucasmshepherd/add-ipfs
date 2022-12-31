af339500051ec534c150e3e388e6053d0df140d3
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-12-31 04:02:26 -0800

    Announce

--- components/discord-announcements.js
+++ components/discord-announcements.js
@@ -31,9 +31,6 @@ export default function News() {
     }
   }
 
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div> 
-
   return (
     <>
       <div className={styles.article}>
