45c81cb3302c4d9502667a98fef83614681467fb
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-12-31 03:57:00 -0800

    About

--- components/about.js
+++ components/about.js
@@ -13,15 +13,8 @@ export default function Donations(props) {
         <div className={styles.content}>
           <h3>DAO Structure & Authority</h3>
           <p>There is no structure.</p>
-          <p>$ADD tokenholders are the collective authority whom make decisions for the DAO <i>through voting</i>. </p>
-          <h3>History</h3>
-          <p><big><b>The Anarchist Development DAO was founded by one guy who believes in a more decentralized society because DYOR is transparency.</b></big></p>
-          <p>Our core principles are <i>trust</i>, free and open markets, private ownership of property and capital, and the complete abolishment of legacy financial systems.</p>
-          <p>The Anarchist Development DAO and its contributors alone have helped execute on this ideology and further the cause of making our mission not just an idea but a reality when I'm not too busy to finish the project.</p>
-          <p>Every individual who has contributed to the Anarchist Development DAO has dedicated most of their professional lives to the advancement of decentralized technologies, activist movements, cryptocurrency, software development or finance which is why I feel terrible for ghosting the entire community.</p>
-
           <h3>dApp Support</h3>
-            <p>x1 Dev Mngr: Prime <span className="note">doxxed</span></p>
+            <p>x1 Dev Mngr: Prime</p>
         </div>
       </div>
     </>
