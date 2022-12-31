ac121dc20771e1993051da74fcc1dedf941cd752
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2022-12-31 02:56:55 -0800

    Push main for disc

--- components/loan.js
+++ components/loan.js
@@ -87,7 +87,7 @@ function MyList() {
               "purpose": "",
               "borrowed": [],
               "loaned": [],
-              "proposal": [],
+              "proposal": [], 
             }
             ])
           }
