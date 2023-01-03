daaacb064dec7cb0b40c568db7b1329cd716059a
Lucas M. Shepherd <lucasmshepherd@gmail.com>
2023-01-02 19:01:06 -0800

    Update discord-announcements.js

--- components/discord-announcements.js
+++ components/discord-announcements.js
@@ -34,28 +34,7 @@ export default function News() {
   return (
     <>
       <div className={styles.article}>
-        {/*<h2 className={styles.title}>Announcements</h2>*/}
-        <div><p>Hi, my name is Brian Ness.  I recently was upset because I fired my lead developer and most of the community left with him.  Turns out nobody would invest in me which is why I need to take advantage of others. When I realized this I immediately raised taxes, cleared the DAO wallet and then I drained the treasury a few days later.  You should know even with all this I still took a loss thanks to Dream and my lack of intelligence.</p>
-        <p>In case you would like to get in touch with me here is some info of mine:</p>
-
-        <ul>
-          <li><b>Name</b>: Brian Ness</li>
-          <li><b>One of my phones</b>: (725) 200-6859</li>
-          <li><b>Addresses found</b>:</li>
-          <li>201 Hay Street Suite 401-B Fayetteville, NC 28301</li>
-          <li>301 Hay Street Fayetteville, NC 28301</li>
-          <li>https://techtacsol.com/contact-us</li>
-          <li><b>Emails</b>:</li>
-          <li>ness.brian1@outlook.com</li>
-          <li>0xethgod0x@protonmail.com</li>
-          <li>0xethgod0x@gmail.com</li>
-          <li><b>Socials</b>:</li>
-          <li>https://twitter.com/NAZZ_NEGG</li>
-          <li>https://twitter.com/NAZZNEGGFLEyRH</li>
-          <li>https://twitter.com/OdaETH</li>
-          <li>https://twitter.com/OdaNobu_ETH</li>
-        </ul>
-
+<div>Thank you for your support. We are incredibly sorry to announce this project is indefinitely on pause.</div>
         </div>
       </div>
     </>
